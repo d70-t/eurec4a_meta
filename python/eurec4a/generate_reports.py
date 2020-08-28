fd2040a5e7f79078cbb2bb7caa72f117488cf0fa
Tobias Kölling <tobi@die70.de>
2020-08-28 15:34:16 +0200

    added basic tex report template

--- python/eurec4a/generate_reports.py
+++ python/eurec4a/generate_reports.py
@@ -8,11 +8,33 @@ html_env = Environment(
     autoescape=select_autoescape(['html', 'xml'])
 )
 
+tex_env = Environment(
+    block_start_string='\BLOCK{',
+    block_end_string='}',
+    variable_start_string='\VAR{',
+    variable_end_string='}',
+    comment_start_string='\#{',
+    comment_end_string='}',
+    line_statement_prefix='%%',
+    line_comment_prefix='%#',
+    trim_blocks=True,
+    autoescape=False,
+    loader=PackageLoader('eurec4a', os.path.join('templates', 'tex')),
+)
+
 def render_instruments(metadata, output_folder):
     instruments = [e for e in metadata.values() if e["type"] == "instrument"]
     print(instruments)
     tpl = html_env.get_template("instruments.html")
     with open(os.path.join(output_folder, "instruments.html"), "w") as outfile:
+        outfile.write(tpl.render(objects=metadata,
+                                 instruments=instruments))
+
+def render_tex_instruments(metadata, output_folder):
+    instruments = [e for e in metadata.values() if e["type"] == "instrument"]
+    print(instruments)
+    tpl = tex_env.get_template("instruments.tex")
+    with open(os.path.join(output_folder, "instruments.tex"), "w") as outfile:
         outfile.write(tpl.render(objects=metadata,
                                  instruments=instruments))
 
@@ -29,6 +51,7 @@ def _main():
     metadata = load_metadata_from_folder(args.metadata_folder)
 
     render_instruments(metadata, args.output_folder)
+    render_tex_instruments(metadata, args.output_folder)
     
 
 if __name__ == "__main__":
