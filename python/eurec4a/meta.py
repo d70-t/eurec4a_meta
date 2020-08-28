ff53fc71b02aacef0c8163ce4a425553c5e204f8
Tobias Kölling <tobi@die70.de>
2020-08-28 17:00:10 +0200

    metadata loader: flatten object hierarchy during load

--- python/eurec4a/meta.py
+++ python/eurec4a/meta.py
@@ -9,6 +9,9 @@ class Annotated:
         self.start = start
         self.end = end
 
+    def replace(self, new):
+        return type(self)(new, self.start, self.end)
+
 class AnnotatedScalar(Annotated):
     def collapse(self):
         return self.child
@@ -20,6 +23,15 @@ class AnnotatedSequence(Annotated):
 class AnnotatedMapping(Annotated):
     def collapse(self):
         return type(self.child)((k.collapse(), v.collapse()) for k, v in self.child.items())
+
+    def __contains__(self, element):
+        return element in {k.collapse() for k in self.child.keys()}
+
+    def __getitem__(self, element):
+        for k, v in self.child.items():
+            if k.collapse() == element:
+                return v
+        raise IndexError(element)
 
 class AnnotatingLoader(BaseLoader):
     def construct_scalar(self, node):
@@ -37,11 +49,26 @@ class AnnotatingLoader(BaseLoader):
 
 def simple_loader(object_type):
     def loader(id, data, location):
+        if "configurations" in data:
+            configurations = data["configurations"]
+            sub_location = location.move_to(configurations.start.line,
+                                            configurations.start.column)
+            sub_loader = get_loader("{}_configurations".format(object_type), sub_location)
+            for name, configuration in configurations.child.items():
+                name = name.replace("{}_{}".format(id.collapse(), name.collapse()))
+                sub_location = location.move_to(configuration.start.line,
+                                                configuration.start.column)
+                configuration.child[name.replace("configuration of")] = id
+                yield from sub_loader(name, configuration, sub_location)
+
         data = data.collapse()
         data["id"] = id.collapse()
         data["type"] = object_type
         if "uris" in data:
             data["uris"] = [parse_uri(uri) for uri in data["uris"]]
+
+        if "configurations" in data:
+            del data["configurations"]
         yield data, location
     return loader
 
@@ -73,6 +100,10 @@ class FileLocation:
         self.filename = filename
         self.line = line
         self.col = col
+
+    def move_to(self, line, col):
+        return FileLocation(self.filename, line, col)
+
     def __str__(self):
         return "{}:{}:{}".format(self.filename, self.line, self.col)
 
