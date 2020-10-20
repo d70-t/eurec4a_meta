f880bb23b8c94469b4be2871588ff05720e7d959
Tobias Kölling <tobias.koelling@mpimet.mpg.de>
2020-10-20 20:53:55 +0200

    compile: create backward links as well

--- python/eurec4a/compile.py
+++ python/eurec4a/compile.py
@@ -1,6 +1,6 @@
 import sys
 import yaml
-from .meta import load_metadata_from_folder
+from .meta import load_metadata_from_folder, create_backward_links
 
 def _main():
     import argparse
@@ -8,7 +8,7 @@ def _main():
     parser.add_argument("folder")
     args = parser.parse_args()
 
-    yaml.dump(load_metadata_from_folder(args.folder), sys.stdout, allow_unicode=True)
+    yaml.dump(create_backward_links(load_metadata_from_folder(args.folder)), sys.stdout, allow_unicode=True)
 
 if __name__ == "__main__":
     _main()
