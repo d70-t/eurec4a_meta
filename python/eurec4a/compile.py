755bd90e6a5590d42904802ce669214f93cd5cf6
Tobias Kölling <tobias.koelling@mpimet.mpg.de>
2020-10-28 11:49:56 +0100

    add compilation into JSON

--- python/eurec4a/compile.py
+++ python/eurec4a/compile.py
@@ -1,14 +1,19 @@
 import sys
 import yaml
+import json
 from .meta import load_metadata_from_folder, create_backward_links
 
 def _main():
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument("folder")
+    parser.add_argument("-j", "--json", default=False, action="store_true")
     args = parser.parse_args()
-
-    yaml.dump(create_backward_links(load_metadata_from_folder(args.folder)), sys.stdout, allow_unicode=True)
+    meta = create_backward_links(load_metadata_from_folder(args.folder))
+    if args.json:
+        json.dump(meta, sys.stdout)
+    else:
+        yaml.dump(meta, sys.stdout, allow_unicode=True)
 
 if __name__ == "__main__":
     _main()
